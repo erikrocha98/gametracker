6d5734b351b0cb2f4c16fccb641dff4365796c1a
Erik Avelino <erikrocha98.jobs@gmail.com>
2025-09-30 21:51:36 -0300

    feat: Implementa index.php com esqueleto para rotas

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
-// Teste básico do ambiente
-phpinfo();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Core\Config;
+use Core\Router;
+use Core\Request;
+
+Config::load();
+
+session_start();
+
+$request = new Request();
+$router = new Router();
+
+require_once __DIR__ . '/../routes/web.php';
+
+$router->dispatch($request);
